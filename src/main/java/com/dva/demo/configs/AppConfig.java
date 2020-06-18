2a9e42df43f34fa8db7dd655428b7e192edb30ea
VasyaDen <vasyaden@mail.ru>
2020-06-18 13:47:12 +0300

    AppConfig added

---
+++ src/main/java/com/dva/demo/configs/AppConfig.java
@@ -0,0 +1,14 @@
+package com.dva.demo.configs;
+
+import com.dva.demo.repository.ParentEntityRepositoryImpl;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages = {"com.dva.demo.repository"}, repositoryBaseClass = ParentEntityRepositoryImpl.class)
+public class AppConfig {
+}
