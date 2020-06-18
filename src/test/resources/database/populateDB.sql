35ba094676f0648f990be514cead70c4c8367db2
VasyaDen <vasyaden@mail.ru>
2020-06-18 18:34:19 +0300

    Create populateDB.sql

---
+++ src/test/resources/database/populateDB.sql
@@ -0,0 +1,3 @@
+insert into groups (group_name, deleted) values ('gr-01', false), ('GR-02', true);
+insert into students (first_name, last_name, deleted, group_id) values ('Ivan', 'Ivanov', false, 1), ('Petr', 'Petrov', true, null);
+
